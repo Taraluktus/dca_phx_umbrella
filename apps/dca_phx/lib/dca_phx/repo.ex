7d125733b94d5f57210e633a193cac3a89573001
Taraluktus <michael.walloschke@googlemail.com>
2024-07-12 13:02:40 +0200

    feat(repo): add module documentation - Add module documentation to the `DcaPhx.Repo` module - Add documentation to the `test_helper.exs` file explaining the purpose of the test setup

--- apps/dca_phx/lib/dca_phx/repo.ex
+++ apps/dca_phx/lib/dca_phx/repo.ex
@@ -1,4 +1,8 @@
 defmodule DcaPhx.Repo do
+  @moduledoc """
+  A module that keeps using definitions for `Ecto.Repo` and friends
+  """
+
   use Ecto.Repo,
     otp_app: :dca_phx,
     adapter: Ecto.Adapters.Postgres
--- apps/dca_phx/test/test_helper.exs
+++ apps/dca_phx/test/test_helper.exs
@@ -1,2 +1,6 @@
+# Starts the ExUnit test framework and sets the Ecto Repo to manual sandbox mode.
+#
+# This is typically used in test setup to ensure each test runs in a clean database
+# state, without affecting other tests.
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(DcaPhx.Repo, :manual)
